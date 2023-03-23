4858b8d21bfd777066a774bbf8fb8f67ef958023
Drfinik <freelanceap@yahoo.com>
2023-03-23 21:31:58 +0300

    I wrote the program according to the task

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿// Написать программу, которая из имеюшегося
+// массива строк формирует массив из строк,
+// длина которых <=3 символа.
+
+void MethodCreateFirstArray(string[] firstArray)
+{
+    Console.Write($"Input array: ");
+    for (int i = 0; i < firstArray.Length; i++) Console.Write($"{firstArray[i]} ");
+    Console.WriteLine();
+}
+
+void MethodCreateSecondArray(string[] secondArray)
+{
+    Console.Write($"Output array: ");
+    string[] newArr = new string[secondArray.Length];
+    for (int i = 0; i < secondArray.Length; i++)
+    {
+        if (secondArray[i].Length <= 3)
+        {
+            newArr[i] = secondArray[i];
+            Console.Write($"{newArr[i]} ");
+        }
+    }
+    Console.WriteLine();
+}
+
+string[] array = { "hello", "2", "world", ":-)"};
+
+Console.WriteLine();
+MethodCreateFirstArray(array);
+Console.WriteLine();
+MethodCreateSecondArray(array);
+Console.WriteLine();
